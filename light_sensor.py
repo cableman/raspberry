f5a9ee3565ff5c529c2757f2b6f8704d1c044909
Jesper Kristensen <cableman@linuxdev.dk>
2013-04-14 00:25:36 +0200

    Added light sensor script

---
+++ light_sensor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+     
+import RPi.GPIO as GPIO, time, os
+     
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+     
+def RCtime (RCpin):
+	reading = 0
+	GPIO.setup(RCpin, GPIO.OUT)
+	GPIO.output(RCpin, GPIO.LOW)
+	time.sleep(0.1)
+
+	GPIO.setup(RCpin, GPIO.IN)
+	# This takes about 1 millisecond per loop cycle
+	while (GPIO.input(RCpin) == GPIO.LOW):
+		reading += 1
+	return reading
+
+try:
+	while True:
+		print RCtime(17)
+
+except KeyboardInterrupt:
+        GPIO.cleanup()
